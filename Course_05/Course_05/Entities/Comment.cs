350f0b1ddc8ff992427d21240bde813472229bca
Vivian Tavares <59315473+viviaantavares@users.noreply.github.com>
2022-06-13 11:59:25 -0300

    Add files via upload

---
+++ Course_05/Course_05/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿namespace Course_05.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+
+
+
+
+    }
+}
